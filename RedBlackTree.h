605bd2406af444eedab1fedc2ddbdbb84eb1d33d
theIdealist <ychou@fandm.edu>
2021-01-26 14:04:48 -0500

    Update header

--- RedBlackTree.h
+++ RedBlackTree.h
@@ -1,19 +1,17 @@
 #define COLOR_RED 0
 #define COLOR_BLACK 1
-
 #include <iostream>
 
 using namespace std;
 
 struct RBTNode {
+	int data;
+	unsigned short int color;
 	RBTNode* left = nullptr;
 	RBTNode* right = nullptr;
 	RBTNode* parent = nullptr;
-	int data;
-	int color;
-	
-	//constructor
-	RBTNode(int data) {
+	// Constructor
+	RBTNode(int data){
 		this->data = data;
 		left = right = parent = nullptr;
 		this->color = COLOR_BLACK;
@@ -24,34 +22,27 @@ struct RBTNode {
 class RedBlackTree {
 	
 	public:
-		RedBlackTree() {
-			root = nullptr;
-			numItems = 0;
-		};
+		RedBlackTree();
 		RedBlackTree(const RedBlackTree& rbt);
-		
-		
 		void Insert(int num);
-		/*
-		bool Contains(int num);
-		int GetMin();
-		int GetMax();
-		int Size();*/
+		void BSTInsert (RBTNode* rn);
+	//	bool Contains(int num);
+	//	int GetMin();
+	//	int GetMax();
+	//	int Size();
+		bool isLeaf(RBTNode* node) const;
+		bool hasLeft(RBTNode* node) const;
+		bool hasRight(RBTNode* node) const;
+		RBTNode* ParentOf(RBTNode* node);
+		string getColor(RBTNode* node) const;
 		// ToString variants
 		string ToInfixString() const { return ToInfixString(root); };
 		string ToPrefixString() const { return ToPrefixString(root); };
-		string ToPostfixString() const { return ToPostfixString(root); };
-	
+
+	//	string ToPostfixString() const { return ToPostfixString(root); };
 	private:
-	
-		RBTNode* root;
+		string ToInfixString (RBTNode* node) const;
+		string ToPrefixString (RBTNode* node) const;
 		unsigned long long int numItems;
-	
-		string getColorNode(RBTNode* node) const;
-		string ToInfixString(RBTNode* node) const;
-		string ToPrefixString(RBTNode* node) const;
-		string ToPostfixString(RBTNode* node) const;
-		
-		RBTNode* InsertHelper(RBTNode* root, RBTNode* newNode);
-		
+		RBTNode* root;
 };
