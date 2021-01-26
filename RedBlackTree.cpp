2cb527cfa1b7aacefd40b0983e5c553bd1d61e33
helia295 <77015184+helia295@users.noreply.github.com>
2021-01-26 15:15:12 +0000

    Add files via upload

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -4,22 +4,114 @@
 
 using namespace std;
 
-const string RedBlackTree::ToInfixString(RBTNode* node){
-	stringstream ss;
-	if (node == nullptr){
-		ss << "";
+
+string RedBlackTree::getColorNode(RBTNode* node) const {
+	
+	if (node->color == 0) {
+		return "R";
 	} else {
-		if (node->left != nullptr){
-			ss << node->left->data << " ";
-		}
-		if (node->right != nullptr){
-			ss << node->right->data << " ";
-		}
+		return "B";
 	}
-		
-	return ss.str();
 }
+
+string RedBlackTree::ToInfixString(RBTNode* node) const {
+	
+	string result;
+	
+	if (node == nullptr){
+		return result;
+	} 
+	ToInfixString((node->left));
+	result += " " + getColorNode(node) + to_string(node->data) + " ";
+	ToInfixString(node->right);
+		
+	return result;
+}
+
+
+string RedBlackTree::ToPrefixString(RBTNode* node) const {
+	
+	string result;
+	
+	if (node == nullptr){
+		return result;
+	} 
+	result += " " + getColorNode(node) + to_string(node->data) + " ";
+	
+	ToPrefixString((node->left));
+	
+	ToPrefixString(node->right);
+		
+	return result;
+	
+}
+
+
+string RedBlackTree::ToPostfixString(RBTNode* node) const {
+	
+	string result;
+	
+	if (node == nullptr){
+		return result;
+	}
+	
+	ToPostfixString((node->left));
+	
+	ToPostfixString(node->right);
+	
+	result += " " + getColorNode(node) + to_string(node->data) + " ";
+		
+	return result;
+	
+}
+
+
+RBTNode* RedBlackTree::InsertHelper(RBTNode* root, RBTNode* newNode) {
+	
+	//if tree is empty, return new node
+	if (root == nullptr) {
+		
+		return newNode;
+	}
+	//else move down the tree (works the same as BST)
+	if (newNode->data < root->data) {
+		
+		root->left = InsertHelper(root->left, newNode);
+		root->left->parent = root;
+		
+	} else if (newNode->data > root->data) {
+		
+		root->right = InsertHelper(root->right, newNode);
+		root->right->parent = root;
+		
+	} else { //if new node = root
+		
+		throw invalid_argument("Duplicate arguments not allowed: " + to_string(newNode->data));
+	}
+	
+	return root; //this is the root we added the new node to (the parent node)
+}
+
+
+
+void RedBlackTree::Insert(int num) {
+	
+	RBTNode* newNode = new RBTNode(num);
+	
+	root = InsertHelper(root, newNode);
+	
+}
+
+/*
 RedBlackTree::RedBlackTree(const RedBlackTree& rbt){
 	rbt.root = this->root;
 	rbt.numItems = this->numItems;
 }
+
+
+void RedBlackTree::Insert(int num) {
+	
+	
+	
+}
+*/
--- RedBlackTree.h
+++ RedBlackTree.h
@@ -1,5 +1,6 @@
 #define COLOR_RED 0
 #define COLOR_BLACK 1
+
 #include <iostream>
 
 using namespace std;
@@ -7,29 +8,50 @@ using namespace std;
 struct RBTNode {
 	RBTNode* left = nullptr;
 	RBTNode* right = nullptr;
+	RBTNode* parent = nullptr;
 	int data;
 	int color;
+	
+	//constructor
+	RBTNode(int data) {
+		this->data = data;
+		left = right = parent = nullptr;
+		this->color = COLOR_BLACK;
+	}
 };
 
 
 class RedBlackTree {
 	
 	public:
-		RedBlackTree(){ root = nullptr; };
+		RedBlackTree() {
+			root = nullptr;
+			numItems = 0;
+		};
 		RedBlackTree(const RedBlackTree& rbt);
-	//	void Insert(int num);
-	//	bool Contains(int num);
-	//	int GetMin();
-	//	int GetMax();
-	//	int Size();
+		
+		
+		void Insert(int num);
+		/*
+		bool Contains(int num);
+		int GetMin();
+		int GetMax();
+		int Size();*/
 		// ToString variants
 		string ToInfixString() const { return ToInfixString(root); };
-		string ToInfixString(RBTNode* node);
-	//	string ToPrefixString() const { return ToPrefixString(root); };
-	//	string ToPostfixString() const { return ToPostfixString(root); };
-	private:
-		//string ToInfixString(RBTNode* node);
-		unsigned long long int numItems;
-		RBTNode* root;
+		string ToPrefixString() const { return ToPrefixString(root); };
+		string ToPostfixString() const { return ToPostfixString(root); };
 	
+	private:
+	
+		RBTNode* root;
+		unsigned long long int numItems;
+	
+		string getColorNode(RBTNode* node) const;
+		string ToInfixString(RBTNode* node) const;
+		string ToPrefixString(RBTNode* node) const;
+		string ToPostfixString(RBTNode* node) const;
+		
+		RBTNode* InsertHelper(RBTNode* root, RBTNode* newNode);
+		
 };
