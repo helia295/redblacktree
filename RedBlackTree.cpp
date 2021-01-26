7c435bbf2fb92d8aa23aa3344cc48efdc40aab9b
theIdealist <ychou@fandm.edu>
2021-01-26 08:43:19 -0500

    Add files via upload

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -1,2 +1,25 @@
-
+#include "RedBlackTree.h"
 #include <iostream>
+#include <sstream>
+
+using namespace std;
+
+const string RedBlackTree::ToInfixString(RBTNode* node){
+	stringstream ss;
+	if (node == nullptr){
+		ss << "";
+	} else {
+		if (node->left != nullptr){
+			ss << node->left->data << " ";
+		}
+		if (node->right != nullptr){
+			ss << node->right->data << " ";
+		}
+	}
+		
+	return ss.str();
+}
+RedBlackTree::RedBlackTree(const RedBlackTree& rbt){
+	rbt.root = this->root;
+	rbt.numItems = this->numItems;
+}
--- RedBlackTree.h
+++ RedBlackTree.h
@@ -1,20 +1,35 @@
 #define COLOR_RED 0
-
+#define COLOR_BLACK 1
 #include <iostream>
+
+using namespace std;
+
+struct RBTNode {
+	RBTNode* left = nullptr;
+	RBTNode* right = nullptr;
+	int data;
+	int color;
+};
+
 
 class RedBlackTree {
 	
 	public:
-		RedBlackTree() {};
+		RedBlackTree(){ root = nullptr; };
 		RedBlackTree(const RedBlackTree& rbt);
-		
-		void Insert();
-		
-	
+	//	void Insert(int num);
+	//	bool Contains(int num);
+	//	int GetMin();
+	//	int GetMax();
+	//	int Size();
+		// ToString variants
+		string ToInfixString() const { return ToInfixString(root); };
+		string ToInfixString(RBTNode* node);
+	//	string ToPrefixString() const { return ToPrefixString(root); };
+	//	string ToPostfixString() const { return ToPostfixString(root); };
 	private:
-	
-		RBTNode* root;
+		//string ToInfixString(RBTNode* node);
 		unsigned long long int numItems;
+		RBTNode* root;
 	
-	
-}
+};
